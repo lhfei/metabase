43f71ef87fc49d387e6f91d9c76cb16d6bbda07f
liushuai <liushuai@qq.com>
2025-06-05 14:02:21 +0800

    修改(隐藏 Examples 事件按钮)

--- frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.tsx
+++ frontend/src/metabase/collections/components/CollectionHeader/CollectionHeader.tsx
@@ -62,9 +62,9 @@ const CollectionHeader = ({
               saveFile={saveFile}
             />
           )}
-          {!isInstanceAnalytics && (
+          {/* {!isInstanceAnalytics && (
             <CollectionTimeline collection={collection} />
-          )}
+          )} */}
           {isInstanceAnalytics && (
             <CollectionPermissions collection={collection} />
           )}
